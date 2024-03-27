e1eed5a822ba94931289d03bc726323d392b8ab1
yakupibrahimbas <ibrahimbasyakup@gmail.com>
2024-03-27 18:26:06 +0300

    m27-4

---
+++ p25.py
@@ -0,0 +1,7 @@
+v1=input("v1:")
+v2=input("v2:")
+
+if v1==v2:
+    print(True)
+else:
+    print(False)
---
+++ p26.py
@@ -0,0 +1,7 @@
+kullanici_adi=input("Lutfen adinizi giriniz:")
+Parola=input("Lutfen parolanizi giriniz")
+
+if kullanici_adi=="admin" and Parola=="1234" :
+    print("Hersey yolunda dostum")
+else:
+    print("Tekrar dene")
