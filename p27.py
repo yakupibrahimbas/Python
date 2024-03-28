1e7405fe86dd49911da193ea5db76792313c02c3
yakupibrahimbas <ibrahimbasyakup@gmail.com>
2024-03-28 10:01:24 +0300

    28-1

---
+++ p27.py
@@ -0,0 +1,7 @@
+#girilen iki sayinin toplami
+sayi1=int(input("Sayi1: "))
+sayi2=int(input("Sayi2: "))
+
+print(sayi1+sayi2)
+
+#int tam sayi turundedir, float virgullu sayi turundedir
---
+++ p28.py
@@ -0,0 +1,5 @@
+sayi1=int(input("Sayi1: "))
+sayi2=int(input("Sayi2: "))
+
+ort=(sayi1+sayi2)/2
+print(ort)
